87e72337fd521c49b4fc2076b90815252e805d11
kongzichixiangjiao <58895347@qq.com>
2017-12-29 09:40:05 +0800

    Initial commit

---
+++ D/D/DView.swift
@@ -0,0 +1,19 @@
+//
+//  DView.swift
+//  D
+//
+//  Created by 侯佳男 on 2017/12/29.
+//  Copyright © 2017年 侯佳男. All rights reserved.
+//
+
+import UIKit
+
+public class DView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
